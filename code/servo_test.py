dccc815a9fbe9408e696157a7bf87530d7f0d562
superlou <lousimons@gmail.com>
2015-08-04 01:19:07 +0000

    Test multiple servos

--- code/servo_test.py
+++ code/servo_test.py
@@ -7,11 +7,18 @@ from servo import Servo
 
 
 if __name__ == "__main__":
-    s1 = Servo(0.15, 0.055, 0.245)
+    # SG92R Servo(0.15, 0.055, 0.245)
+    s1 = Servo(0.15, 0.065, 0.225)  # HS-311
+    s2 = Servo(0.15, 0.065, 0.225)
+    s3 = Servo(0.15, 0.065, 0.225)
     pbs = PiBlasterServos()
     pbs.add(s1, 23)
+    pbs.add(s2, 24)
+    pbs.add(s3, 18)
 
     pbs[0] = 0.0
+    pbs[1] = 0.0
+    pbs[2] = 0.0
 
     def on_terminate(signal, frame):
         pbs.turn_off()
@@ -19,11 +26,13 @@ if __name__ == "__main__":
 
     signal.signal(signal.SIGINT, on_terminate)
 
-    i = 0
-    fs = 20.
+    i = 0.
+    fs = 100.
 
     while(1):
-        pbs[0] = pi/2.0 * sin(2.*pi*1.*i/fs)
+        pbs[0] = pi/2.0 * sin(2.*pi*0.5*i/fs)
+        pbs[1] = pi/2.0 * sin(2.*pi*0.5*i/fs)
+        pbs[2] = pi/2.0 * sin(2.*pi*0.5*i/fs)
         i = i + 1
 
         time.sleep(1./fs)
