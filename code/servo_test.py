6425d96613b963f8935ea04928d505c182d409f1
superlou <lousimons@gmail.com>
2015-08-21 01:05:11 +0000

    Adding 4th servo

--- code/servo_test.py
+++ code/servo_test.py
@@ -7,18 +7,20 @@ from servo import Servo
 
 
 if __name__ == "__main__":
-    # SG92R Servo(0.15, 0.055, 0.245)
-    s1 = Servo(0.15, 0.065, 0.225)  # HS-311
+    s0 = Servo(0.15, 0.065, 0.225)  # HS-311
+    s1 = Servo(0.15, 0.065, 0.225)
     s2 = Servo(0.15, 0.065, 0.225)
-    s3 = Servo(0.15, 0.065, 0.225)
+    s3 = Servo(0.15, 0.055, 0.245)  # SG92R
     pbs = PiBlasterServos()
+    pbs.add(s0, 18)
     pbs.add(s1, 23)
     pbs.add(s2, 24)
-    pbs.add(s3, 18)
+    pbs.add(s3, 25)
 
     pbs[0] = 0.0
     pbs[1] = 0.0
     pbs[2] = 0.0
+    pbs[3] = 0.0
 
     def on_terminate(signal, frame):
         pbs.turn_off()
@@ -28,14 +30,6 @@ if __name__ == "__main__":
 
     i = 0.
     fs = 100.
-
-    # while(1):
-    #     pbs[0] = 0.7 * pi/2.0 * sin(2.*pi*0.5*i/fs)
-    #     pbs[1] = 0.7 * pi/2.0 * sin(2.*pi*0.5*i/fs)
-    #     pbs[2] = 0.7 * pi/2.0 * sin(2.*pi*0.5*i/fs)
-    #     i = i + 1
-    #
-    #     time.sleep(1./fs)
 
     while(1):
         text = raw_input("> ")
@@ -70,6 +64,14 @@ if __name__ == "__main__":
 
                 i = i + 1
                 time.sleep(1./fs)
+
+        elif "shake" in text:
+            i = 0
+            rate = 3
+            while(i < (fs / rate * 2)):
+                pbs[3] = 0.2 * pi/2.0 * sin(2.*pi*rate*i/fs)
+                i = i + 1
+                time.sleep(1./fs)
         elif "tilt" in text:
             i = 0
             rate = 2
@@ -87,6 +89,7 @@ if __name__ == "__main__":
                 pbs[0] = 0.2 * pi/2.0 * sin(2.*pi*rate*i/fs)
                 pbs[1] = 0.2 * pi/2.0 * sin(2.*pi*rate*i/fs + pi / 3.)
                 pbs[2] = 0.2 * pi/2.0 * sin(2.*pi*rate*i/fs + 2. * pi / 3.)
+                pbs[3] = -0.4 * pi/2.0 * sin(2.*pi*rate*i/fs)
 
                 i = i + 1
                 time.sleep(1./fs)
