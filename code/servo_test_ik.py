2da1a193463b7900a539d74e3475d95cd0819fe5
superlou <lousimons@gmail.com>
2015-11-19 01:57:37 +0000

    Curses interface

--- code/servo_test_ik.py
+++ code/servo_test_ik.py
@@ -3,7 +3,7 @@ import numpy as np
 from pi_blaster_servos import PiBlasterServos
 from servo import Servo
 from kinematics.inverse.full_ik import FullIK
-import cProfile as profile
+import curses
 
 
 def rotate_vector(vector, axis, angle):
@@ -19,6 +19,43 @@ def rotate_vector(vector, axis, angle):
     return v_rot
 
 
+def reset_servos(pbs):
+    pbs[0] = 0
+    pbs[1] = 0
+    pbs[2] = 0
+    pbs[3] = 0
+
+
+def set_servo_angles(pbs, angles):
+    if not any(np.isnan(angles)):
+        pbs[0] = angles[0]
+        pbs[1] = angles[1]
+        pbs[2] = angles[2]
+        pbs[3] = angles[3]
+
+
+def solve_servos(pbs, ik, e_v, f_v, height):
+    angles = full_ik.solve(e_v, f_v, height)
+    set_servo_angles(pbs, angles)
+
+
+def pan_global(e_v, f_v, angle):
+    gz_v = np.array([0, 0, 1])
+    e_v = rotate_vector(e_v, gz_v, angle)
+    f_v = rotate_vector(f_v, gz_v, angle)
+    return e_v, f_v
+
+
+def pitch_local(e_v, f_v, angle):
+    e_v = rotate_vector(e_v, f_v, angle)
+    return e_v, f_v
+
+
+def roll_local(e_v, f_v, angle):
+    f_v = rotate_vector(f_v, e_v, angle)
+    return e_v, f_v
+
+
 if __name__ == "__main__":
     s0 = Servo(0.15, 0.065, 0.225)  # HS-311
     s1 = Servo(0.15, 0.065, 0.225)
@@ -30,37 +67,57 @@ if __name__ == "__main__":
     pbs.add(s2, 24)
     pbs.add(s3, 25)
 
-    pbs[0] = 0
-    pbs[1] = 0
-    pbs[2] = 0
-    pbs[3] = np.radians(-45)
+    reset_servos(pbs)
 
     def on_terminate(signal, frame):
+        curses.endwin()
+        reset_servos(pbs)
         pbs.turn_off()
         sys.exit()
 
     signal.signal(signal.SIGINT, on_terminate)
 
-    gx_v = np.array([1, 0, 0])
-    gy_v = np.array([0, 1, 0])
-    gz_v = np.array([0, 0, 1])
-
-    e_v = np.array([1, 0, 0])
-    f_v = np.array([0, 1, 0])
-
-    e_v = rotate_vector(e_v, f_v, np.radians(0))
-    e_v = rotate_vector(e_v, gz_v, np.radians(45))
-    f_v = rotate_vector(f_v, gz_v, np.radians(45))
-    f_v = rotate_vector(f_v, e_v, np.radians(-30))
 
     full_ik = FullIK(platform_radius=1.42, neck_length=2.04, base_radius=3.35,
                      lower_leg_length=2.37, upper_leg_length=3.77)
-    angles = full_ik.solve(e_v, f_v, 5.0)
 
-    print angles
 
-    if not any(np.isnan(angles)):
-        pbs[0] = angles[0]
-        pbs[1] = angles[1]
-        pbs[2] = angles[2]
-        pbs[3] = angles[3]
+    e_v = np.array([1, 0, 0])
+    f_v = np.array([0, 1, 0])
+    height = 5
+    solve_servos(pbs, full_ik, e_v, f_v, height)
+
+    stdscr = curses.initscr()
+    curses.cbreak()
+    stdscr.keypad(1)
+
+    stdscr.addstr(0, 0, "Press \"q\" to quit")
+    stdscr.refresh()
+
+    key = ''
+    while key != ord('q'):
+        key = stdscr.getch()
+        stdscr.addch(1, 0, key)
+        stdscr.refresh()
+
+        if key == ord("w"):
+            height += 0.2
+        elif key == ord("s"):
+            height -= 0.2
+        elif key == curses.KEY_LEFT:
+            e_v, f_v = pan_global(e_v, f_v, np.radians(5))
+        elif key == curses.KEY_RIGHT:
+            e_v, f_v = pan_global(e_v, f_v, np.radians(-5))
+        elif key == curses.KEY_UP:
+            e_v, f_v = pitch_local(e_v, f_v, np.radians(2))
+        elif key == curses.KEY_DOWN:
+            e_v, f_v = pitch_local(e_v, f_v, np.radians(-2))
+        elif key == ord("a"):
+            e_v, f_v = roll_local(e_v, f_v, np.radians(-2))
+        elif key == ord("d"):
+            e_v, f_v = roll_local(e_v, f_v, np.radians(2))
+
+        solve_servos(pbs, full_ik, e_v, f_v, height)
+
+    curses.endwin()
+
