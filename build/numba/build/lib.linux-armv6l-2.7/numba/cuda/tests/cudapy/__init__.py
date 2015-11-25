7ba121d273d14da64d2103cfb561688b2d117494
superlou <lousimons@gmail.com>
2015-11-25 00:00:47 +0000

    Remove temporary build files

--- build/numba/build/lib.linux-armv6l-2.7/numba/cuda/tests/cudapy/__init__.py
+++
@@ -1,5 +0,0 @@
-from numba.tests import SerialSuite
-
-def load_tests(loader, tests, pattern):
-    suite = loader.discover("numba.cuda.tests.cudapy")
-    return SerialSuite(suite)
--- build/numba/build/lib.linux-armv6l-2.7/numba/datamodel/__init__.py
+++
@@ -1,4 +0,0 @@
-from .manager import DataModelManager
-from .argpacker import ArgPacker
-from .registry import register_default, default_manager, register
-from .models import PrimitiveModel, CompositeModel, StructModel
